12ab60007b9d496754edf99c7f4427c22b073c98
Sgissa <salmanegarba721@gmail.com>
2026-02-14 13:22:29 -0500

    content card fixed to fix kanban style

--- components/ContentCard.jsx
+++ components/ContentCard.jsx
@@ -1,10 +1,11 @@
-export default function ContentCard({ title, platform }) {
+export default function ContentCard({ title, platform, status }) {
   return (
     <>
-      <div className="card h-100 shadow-sm border-0 app-banner">
+      <div className="card mb-3 shadow-sm border-0 app-banner">
         <div className="card-body">
           <h5 className="card-title fw-semibold">{title}</h5>
           <p className="card-text text-secondary small mb-0 ">{platform}</p>
+          <p className="card-text text-secondary small mb-0 ">{status}</p>
         </div>
       </div>
     </>
